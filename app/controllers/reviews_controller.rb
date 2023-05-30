b7fdf441669eb05c8c4ad602a2b634e0950459ae
William Foulks <wjfoulks@gmail.com>
2023-05-30 15:27:25 +0800

    Added code to each method in reviews controller.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,27 @@
 class ReviewsController < ApplicationController
+
+  def index
+    @reviews = Reviews.all
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @transaction = Transaction.find(params[:transaction_id])
+    @review = Review.new(review_params)
+    @review.transaction = @transaction
+    if @review.save
+      redirect_to transaction_path(@transaction)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end
