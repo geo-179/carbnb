7fec38d1483cf642c7a51ef96ea997c391446044
William Foulks <wjfoulks@gmail.com>
2023-05-30 13:09:26 +0800

    Added transaction model with validations.  Created empty transaction controller.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,6 @@
 class Car < ApplicationRecord
   belongs_to :user
+  has_many :transactions
   validates :price, presence: true, numericality: { only_integer: true }
   validates :location, presence: true, inclusion: { in: ['Los Angeles', 'Miami', 'Honolulu', 'Sydney', 'Shanghai', 'London'] }
   validates :model, presence: true
