9e87013a2a74dac627eab0334aa7c22bfd8295c7
William Foulks <wjfoulks@gmail.com>
2023-05-30 11:50:24 +0800

    Added validations to Car model

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,5 @@
 class Car < ApplicationRecord
   belongs_to :user
+  validates :price, presence: true, numericality: { only_integer: true }
+  validates :model, :location, presence: true, inclusion: { in: ['Los Angeles', 'Miami', 'Honolulu', 'Sydney', 'Shanghai', 'London'] }
 end
