5d331696ebbcf0376555731fac2aa12dafde8666
William Foulks <wjfoulks@gmail.com>
2023-05-30 15:49:50 +0800

    Added set user to new and create method.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :set_transaction, only: [:new, :create]
+  before_action :set_transaction_user, only: [:new, :create]
 
   # Did not add view page for index as I assume we will be displaying reviews on a users or cars page.
   def index
@@ -13,6 +13,7 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.transaction = @transaction
+    @review.user = @user
     if @review.save
       redirect_to transaction_path(@transaction)
     else
@@ -24,6 +25,7 @@ class ReviewsController < ApplicationController
 
   def set_transaction
     @transaction = Transaction.find(params[:transaction_id])
+    @user = User.find(params[:user_id])
   end
   def review_params
     params.require(:review).permit(:rating, :content)
