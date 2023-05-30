c3f577f5cf00dfb867255e8bccecc3b417b2a830
Gio Redacted <gio.huh.179@gmail.com>
2023-05-30 16:35:16 +0800

    Added association for scheduled, in_progress, and completed

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,7 +1,9 @@
 class Car < ApplicationRecord
   belongs_to :user
   has_many :transactions, foreign_key: :car_id
-  has_many :rented_users, -> { where(transactions: { status: 'scheduled' }) }, through: :transactions, source: :user
+  has_many :scheduled_users, -> { where(transactions: { status: 'scheduled' }) }, through: :transactions, source: :user
+  has_many :in_progress_users, -> { where(transactions: { status: 'in progress' }) }, through: :transactions, source: :user
+  has_many :completed_users, -> { where(transactions: { status: 'completed' }) }, through: :transactions, source: :user
   validates :price, presence: true, numericality: { only_integer: true }
   validates :location, presence: true, inclusion: { in: ['Los Angeles', 'Miami', 'Honolulu', 'Sydney', 'Shanghai', 'London'] }
   validates :model, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :cars
   has_many :transactions
-  has_many :rented_cars, -> { where(transactions: { status: 'scheduled' }) }, through: :transactions, source: :car
+  has_many :scheduled_cars, -> { where(transactions: { status: 'scheduled' }) }, through: :transactions, source: :car
+  has_many :in_progress_cars, -> { where(transactions: { status: 'in progress' }) }, through: :transactions, source: :car
+  has_many :completed_cars, -> { where(transactions: { status: 'completed' }) }, through: :transactions, source: :car
   has_many :reviews
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
