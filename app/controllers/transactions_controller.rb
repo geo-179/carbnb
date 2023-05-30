d075068f85580005f0d9ff2ac8544b994e64ff77
William Foulks <wjfoulks@gmail.com>
2023-05-30 14:17:17 +0800

    Wrote code for the methods in the transaction controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,37 @@
 class TransactionsController < ApplicationController
+  before_action :set_transaction, only: [:show, :destroy]
+
+  def index
+    @transactions = Transaction.all
+  end
+
+  def show
+  end
+
+  def new
+    @transaction = Transaction.new
+  end
+
+  def create
+    @transaction = Transaction.new(transaction_params)
+    if @transaction.save
+      redirect_to transaction_path(@transaction)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @transaction.destroy
+  end
+
+  private
+
+  def set_transaction
+    @transaction = Transaction.find(params[:id])
+  end
+
+  def transaction_params
+    params.require(:transaction).permit(:start_date, :end_date, :status)
+  end
 end
