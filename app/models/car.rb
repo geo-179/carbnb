d1928f61385743028911c1faf0b9ca11b87ac7f9
Gio Redacted <gio.huh.179@gmail.com>
2023-05-31 11:09:08 +0800

    Added checker in Transactions if two users are the same. Renamed user and cars to owner and owned_cars.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,5 +1,5 @@
 class Car < ApplicationRecord
-  belongs_to :user
+  belongs_to :owner, class_name: "User", inverse_of: :owned_cars, foreign_key: "user_id"
   has_many :transactions, foreign_key: :car_id
   has_many :scheduled_users, -> { where(transactions: { status: 'scheduled' }) }, through: :transactions, source: :user
   has_many :in_progress_users, -> { where(transactions: { status: 'in progress' }) }, through: :transactions, source: :user
