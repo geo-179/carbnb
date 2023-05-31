5bfa99d0eaf2dcb91b37e09d32293495b17110fb
Gio Redacted <gio.huh.179@gmail.com>
2023-05-31 14:25:26 +0800

    Added Car form and initiated setting for cloudinary image hosting

--- app/models/car.rb
+++ app/models/car.rb
@@ -4,6 +4,8 @@ class Car < ApplicationRecord
   has_many :scheduled_users, -> { where(transactions: { status: 'scheduled' }) }, through: :transactions, source: :user
   has_many :in_progress_users, -> { where(transactions: { status: 'in progress' }) }, through: :transactions, source: :user
   has_many :completed_users, -> { where(transactions: { status: 'completed' }) }, through: :transactions, source: :user
+  has_many_attached :photos
+
   validates :price, presence: true, numericality: { only_integer: true }
   validates :location, presence: true, inclusion: { in: ['Los Angeles', 'Miami', 'Honolulu', 'Sydney', 'Shanghai', 'London'] }
   validates :model, presence: true
