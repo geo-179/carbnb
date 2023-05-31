43ae32676bdf9f807502c7f3a2eb9901f8d2c588
William Foulks <wjfoulks@gmail.com>
2023-05-31 18:18:57 +0800

    Created User profile page with HTML and Tailwind. . Did not add rental history section yet.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,8 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    @user = current_user
+    @user = User.find(params[:id])  # For testing purposes.  Erase this line and use line below before deploying
+    # @user = current_user    USE THIS LINE AND DELETE LINE ABOVE BEFORE DEPLOYING
     authorize @user
   end
 end
