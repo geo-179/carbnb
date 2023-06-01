b7c7ac057d620af446c6c15dde86a5bfab47bcfa
Gio Redacted <gio.huh.179@gmail.com>
2023-06-01 15:42:13 +0800

    Popup Map

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -7,7 +7,8 @@ class CarsController < ApplicationController
     @markers = @cars.geocoded.map do |car|
       {
         lat: car.latitude,
-        lng: car.longitude
+        lng: car.longitude,
+        info_window_html: render_to_string(partial: "info_window", locals: {car: car})
       }
     end
   end
