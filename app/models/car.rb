7ec93b72cd8f756a9c023ee78f902501fdf7d39a
Gio Redacted <gio.huh.179@gmail.com>
2023-06-01 13:47:53 +0800

    Added finer grain associations for user and car's transactions and initialized geocoding

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,6 +1,11 @@
 class Car < ApplicationRecord
   belongs_to :owner, class_name: "User", inverse_of: :owned_cars, foreign_key: "user_id"
   has_many :transactions, foreign_key: :car_id
+
+  has_many :scheduled_transactions, -> { where(status: 'scheduled') }, class_name: 'Transaction', foreign_key: :car_id
+  has_many :in_progress_transactions, -> { where(status: 'in progress') }, class_name: 'Transaction', foreign_key: :car_id
+  has_many :completed_transactions, -> { where(status: 'completed') }, class_name: 'Transaction', foreign_key: :car_id
+
   has_many :scheduled_users, -> { where(transactions: { status: 'scheduled' }) }, through: :transactions, source: :user
   has_many :in_progress_users, -> { where(transactions: { status: 'in progress' }) }, through: :transactions, source: :user
   has_many :completed_users, -> { where(transactions: { status: 'completed' }) }, through: :transactions, source: :user
@@ -9,4 +14,11 @@ class Car < ApplicationRecord
   validates :price, presence: true, numericality: { only_integer: true }
   validates :location, presence: true, inclusion: { in: ['Los Angeles', 'Miami', 'Honolulu', 'Sydney', 'Shanghai', 'London'] }
   validates :model, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_location?
+
+  def address
+    location
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,20 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :owned_cars, class_name: "Car", inverse_of: :owner
-  has_many :transactions
+  has_many :transactions, foreign_key: :user_id
+
+  def scheduled_transactions
+    transactions.where(status: 'scheduled')
+  end
+
+  def in_progress_transactions
+    transactions.where(status: 'in progress')
+  end
+
+  def completed_transactions
+    transactions.where(status: 'completed')
+  end
+
   has_many :scheduled_cars, -> { where(transactions: { status: 'scheduled' }) }, through: :transactions, source: :car
   has_many :in_progress_cars, -> { where(transactions: { status: 'in progress' }) }, through: :transactions, source: :car
   has_many :completed_cars, -> { where(transactions: { status: 'completed' }) }, through: :transactions, source: :car
